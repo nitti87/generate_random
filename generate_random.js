9b2d76ca2381075ab17c18887f88b9996a936be7
Arijanit Salihu <44554259+nitti87@users.noreply.github.com>
2021-09-28 11:16:23 +0200

    Made it async

--- generate_random.js
+++ generate_random.js
@@ -1,21 +1,23 @@
 const { hash } = require('@db/mysql_funct') // for private use
 
 const generate_random = async ({hashCheck = false, uniqueAgainst, char_length = 6}) => {
-  let [generate, allowChar] = ['', 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@£€_ -[]&']
+  return new Promise((resolve, reject) => {
+    let [generate, allowChar] = ['', 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@£€_ -[]&']
 
-  const iterate = () => {
-    while (true) {
-      for (let i = 0; i <= char_length; ++i) {
-        generate += allowChar.charAt(Math.floor(Math.random() * allowChar.length))
-      }
+    const iterate = async () => {
+      while (true) {
+        for (let i = 0; i <= char_length; ++i) {
+          generate += allowChar.charAt(Math.floor(Math.random() * allowChar.length))
+        }
 
-      if (uniqueAgainst && (hashCheck ? hash(generate, true) : generate) !== uniqueAgainst) {
-        return generate
-      } else if (!uniqueAgainst) {
-        return generate
+        if (uniqueAgainst && (hashCheck ? hash(generate, true) : generate) !== uniqueAgainst) {
+          return generate
+        } else if (!uniqueAgainst) {
+          return generate
+        }
       }
     }
-  }
 
-  return iterate()
+    return iterate().then(d => { resolve(d) })
+  })
 }
